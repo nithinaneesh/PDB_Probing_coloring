8c4722ff28073b2f83d6aa5c7a1569c0ed7cb969
twirecki <twirecki@genesilico.pl>
2020-04-27 21:03:22 +0200

    ver 0.8

--- probing_bfactor_pdb.py
+++ probing_bfactor_pdb.py
@@ -19,8 +19,8 @@ def argument_parser():
                         help="File with reactivity")
     parser.add_argument("-o", "--output_pdb", required=False, dest="pdb_out",
                         help="Name of output PDB file.")
-    parser.add_argument("-c", "--color", required=False, dest="color", default='default', choices=['yellow', 'pink',
-                        'spring', 'rainbow', 'gyr', 'grey', 'blue', 'red', 'green', 'default'],
+    parser.add_argument("-c", "--color_by", required=False, dest="color", default='all', choices=['all', 'base',
+                        'back', 'sugar','basesug', 'backsug'],
                         help="Color of output images")
 
 
@@ -29,13 +29,101 @@ def argument_parser():
     in_pdb = args.pdb_in
     out_pdb = args.pdb_out
     reactivity = args.react_in
-    return in_pdb, out_pdb, reactivity
+    color_by = args.color
+    return in_pdb, out_pdb, reactivity, color_by
+
+
+def change_bfactor():
+    
+    print(pdb_in_list)
+    print(reactivities_df)
+    print(reactivities_df.shape[0])
+
+    backbone  = ["P  ", "OP1", "OP2", "O3'", "O5'", "O3*", "O5*", "C3'", "C4'", "C5'", "C3*", "C4*", "C5*"]
+    base = ["N9 ", "C8 ", "N7 ", "C5 ", "C4 ", "N3 ", "O2 ", "N2 ", "N1 ", "C6 ", "O6 ", "C2 ", "N6 ", "O4 ", "N4 "]
+    sugar = ["C3'", "C4'", "C5'", "C3*", "C4*", "C5*", "C1'", "C1*", "C2'", "C2*", "O2'", "O2*", "O4'","O4*"]
+
+    if color_by == 'all':
+        color = backbone + base + sugar
+    elif color_by == 'base':
+        color = base
+    elif color_by == 'back':
+        color = backbone
+    elif color_by == 'sugar':
+        color = sugar
+    elif color_by == 'basesug':
+        color = base + sugar
+    elif color_by == 'backsug':
+        color = backbone + sugar
+        
+    pdb_out_list = pdb_in_list.copy()
+    
+    
+    kok = '  23'
+    lol = int(kok)
+    print(lol)
+    
+
+    for i in range(0, reactivities_df.shape[0]):
+        #print(i+1)
+        for k in range(0, len(pdb_in_list)):
+            #print(pdb_in_list[k][13:16])
+            if int(pdb_in_list[k][22:26]) == i:
+                line = list(pdb_out_list[k])
+                if pdb_in_list[k][13:16] in color:
+                    #line = list(pdb_out_list[k])
+                    #print(pdb_out_list[k])
+                    #print(line[60:66])
+                    #print(list(str(reactivities_df.iloc[i]["react"])))
+                    re = list(str("%.2f" % reactivities_df.iloc[i]["react"]))
+                    #print("%.2f" % reactivities_df.iloc[i]["react"])
+                    #print(pdb_in_list[k][60:66])
+                    #print([" ", " "]+re) 
+                    put = [" ", " "]+re
+                    line[60:66] = put
+                    #print(put)
+                    #print(''.join(line))
+                    line = ''.join(line)
+                    #print('23 uuuu', i, k)
+                    #pdb_out_list[k] = line
+                    #pdb_out_list[k][60:66] = ''
+                else:
+                    put = [" ", " ", "1", ".", "2", "5"]
+                    line[60:66] = put
+                    line = ''.join(line)
+                pdb_out_list[k] = line
+       
+    print(reactivities_df)
+    print(reactivities_df.iloc[10]["react"])
+
+    print(pdb_out_list)
+    out= open("out.pdb", "w")
+    for i in pdb_out_list:
+        out.write(i)
+    out.close()
+
 
 
 def read_pdb():
 
     print('kupka')
-
+    pdb_list = []
+    with open(in_pdb) as f:
+        for line in f:
+            if (line[:6] == "ATOM  " and line[16] != "B" and line[16] != "G") or (line[:6] == "HETATM" and line[17:20] == "GTP" \
+                and line[15] != "B" and line[15] != "G" and line[14] != "B" and line[14] != "G" \
+                and line[16] != "B" and line[16] != "G"):
+                print('atom')
+                pdb_list.append(line)
+    
+    '''
+    out= open("out.pdb", "w")
+    for i in pdb_list:
+        out.write(i)
+    out.close()
+    '''
+    #print(pdb_list)
+    return pdb_list    
 
 '''
 
@@ -63,14 +151,37 @@ COLUMNS        DATA  TYPE    FIELD        DEFINITION
 
 '''
 
+def read_react():
+    
+    
+    with open(reactivity) as f:
+        count = sum(1 for _ in f)
+#    print(count)
+    
+    if count >2:
+        react_df = pd.read_csv(reactivity, sep=' ',index_col=False,header=None, names=["num","react"])
+        react_df.set_index("num", inplace=True)
+        print('from columns')
+    else:
+        react_df = pd.read_csv(reactivity, sep='\t',index_col=False,header=None)
+        react_df = react_df.transpose()[0]
+        react_df = pd.DataFrame(react_df)
+        react_df.index.name="num"
+        react_df.index += 1
+        react_df.columns = ["react"]
+        print('from sheet')    
+#    print(react_df, type(react_df))
 
-
+    return react_df
 
 
 if __name__ == '__main__':
 
-    in_pdb, out_pdb, reactivity = argument_parser()
+    in_pdb, out_pdb, reactivity, color_by = argument_parser()
 
-    read_pdb()
+    pdb_in_list = read_pdb()
+    reactivities_df = read_react()
+    
+    change_bfactor()    
 
 
