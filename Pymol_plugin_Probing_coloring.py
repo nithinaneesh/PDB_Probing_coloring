017c5d895e1f2873319fc567def079ca61f0ad6a
twirecki <twirecki@genesilico.pl>
2020-05-28 15:59:32 +0200

    new coloring option

--- Pymol_plugin_Probing_coloring.py
+++ Pymol_plugin_Probing_coloring.py
@@ -5,6 +5,52 @@ from pymol import cmd
 import math
 
 
+
+def align_all( subset = [] ):
+  """
+  Superimpose all open models onto the first one.
+  This may not work well with selections.
+
+  This function is probably taken from https://daslab.stanford.edu/site_data/docs_pymol_rhiju.pdf
+  """
+  print("""This returns a list with 7 items:
+
+    RMSD after refinement
+    Number of aligned atoms after refinement
+    Number of refinement cycles
+    RMSD before refinement
+    Number of aligned atoms before refinement
+    Raw alignment score
+    Number of residues aligned """)
+
+  AllObj=cmd.get_names("all")
+  for x in AllObj[1:]:
+    #print(AllObj[0],x)
+    subset_tag = ''
+    if isinstance( subset, int ):
+      subset_tag = ' and resi %d' % subset
+    elif isinstance( subset, list ) and len( subset ) > 0:
+      subset_tag = ' and resi %d' % (subset[0])
+      for m in range( 1,len(subset)): subset_tag += '+%d' % subset[m]
+    elif isinstance( subset, str ) and len( subset ) > 0:
+      subset_tag = ' and %s' % subset
+    values = cmd.align(x+subset_tag,AllObj[0]+subset_tag)
+    print(AllObj[0], x, ' '.join([str(v) for v in values]), '-- RMSD', values[3], ' of ', values[6], 'residues')
+    cmd.zoom()
+
+def corona():
+
+    cmd.hide("sticks", "all")
+    cmd.hide("lines", "all")
+    cmd.show("cartoon", "all")
+    cmd.set("cartoon_ring_mode", 3)
+    cmd.set("cartoon_ring_finder", 2)
+    cmd.set("cartoon_ladder_mode", 2)
+    cmd.set("cartoon_ring_transparency", 0.30)
+    #cmd.spectrum('b', 'white_paleyellow_yellow_red_brown_grey', minimum=-0.25, maximum=1.25, byres=1)
+    cmd.spectrum('b', 'white_yellow_red_brown', minimum=0, maximum=100, byres=1)
+
+
 def shape():
 
     cmd.hide("sticks", "all")
@@ -54,10 +100,13 @@ else:
     print('shape')
     print('dms')
     print('cmct')
-
+    print('aa')
+    
     cmd.extend('shape', shape)
     cmd.extend('dms', dms)
     cmd.extend('cmct', cmct)
+    cmd.extend('aa', align_all)
+    cmd.extend('gocoronago', corona)
 
 
     # set dash lines
